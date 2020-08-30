2981d92840d0d3793803d086bf23076a47ca34c4
Trey Dockendorf <treydock@gmail.com>
2020-08-30 15:45:41 -0400

    Add puppet strings docs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,236 @@
 # @summary Manages a Patroni instance
 #
+# @param scope
+#   Refer to Patroni Global `scope` setting
+# @param namespace
+#   Refer to Patroni Global `namespace` setting
+# @param hostname
+#   Refer to Patroni Global `name` setting
+# @param dcs_loop_wait
+#   Refer to Patroni Dynamic Configuration Settings `loop_wait` setting
+# @param dcs_ttl
+#   Refer to Patroni Dynamic Configuration Settings `ttl` setting
+# @param dcs_retry_timeout
+#   Refer to Patroni Dynamic Configuration Settings `retry_timeout` setting
+# @param dcs_maximum_lag_on_failover
+#   Refer to Patroni Dynamic Configuration Settings `maximum_lag_on_failover` setting
+# @param dcs_master_start_timeout
+#   Refer to Patroni Dynamic Configuration Settings `master_start_timeout` setting
+# @param dcs_synchronous_mode
+#   Refer to Patroni Dynamic Configuration Settings `synchronous_mode` setting
+# @param dcs_synchronous_mode_strict
+#   Refer to Patroni Dynamic Configuration Settings `synchronous_mode_strict` setting
+# @param dcs_postgresql_use_pg_rewind
+#   Refer to Patroni Dynamic Configuration Settings `postgresql_use_pg_rewind` setting
+# @param dcs_postgresql_use_slots
+#   Refer to Patroni Dynamic Configuration Settings `postgresql_use_slots` setting
+# @param dcs_postgresql_recovery_conf
+#   Refer to Patroni Dynamic Configuration Settings `postgresql_recovery_conf` setting
+# @param dcs_postgresql_parameters
+#   Refer to Patroni Dynamic Configuration Settings `postgresql_parameters` setting
+# @param bootstrap_method
+#   Refer to Bootstrap configuration settings `method` setting
+# @param initdb_data_checksums
+#   Refer to Bootstrap configuration settings `data-checksums` setting
+# @param initdb_encoding
+#   Refer to Bootstrap configuration settings `encoding` setting
+# @param initdb_locale
+#   Refer to Bootstrap configuration settings `locale` setting
+# @param bootstrap_pg_hba
+#   Refer to Bootstrap configuration settings `pg_hba` setting
+# @param bootstrap_users
+#   Refer to Bootstrap configuration settings `users` setting
+# @param bootstrap_post_bootstrap
+#   Refer to Bootstrap configuration settings `post_bootstrap` setting
+# @param bootstrap_post_init
+#   Refer to Bootstrap configuration settings `post_init` setting
+# @param superuser_username
+#   Refer to PostgreSQL configuration settings superuser username
+# @param superuser_password
+#   Refer to PostgreSQL configuration settings superuser password
+# @param replication_username
+#   Refer to PostgreSQL configuration settings replication username
+# @param replication_password
+#   Refer to PostgreSQL configuration settings replication password
+# @param callback_on_reload
+#   Refer to PostgreSQL configuration settings callbacks `on_reload`
+# @param callback_on_restart
+#   Refer to PostgreSQL configuration settings callbacks `on_restart`
+# @param callback_on_role_change
+#   Refer to PostgreSQL configuration settings callbacks `on_role_change`
+# @param callback_on_start
+#   Refer to PostgreSQL configuration settings callbacks `on_start`
+# @param callback_on_stop
+#   Refer to PostgreSQL configuration settings callbacks `on_stop`
+# @param pgsql_connect_address
+#   Refer to PostgreSQL configuration settings `connect_address` setting
+# @param pgsql_create_replica_methods
+#   Refer to PostgreSQL configuration settings `create_replica_methods` setting
+# @param pgsql_data_dir
+#   Refer to PostgreSQL configuration settings `data_dir` setting
+# @param pgsql_config_dir
+#   Refer to PostgreSQL configuration settings `config_dir` setting
+# @param pgsql_bin_dir
+#   Refer to PostgreSQL configuration settings `bin_dir` setting
+# @param pgsql_listen
+#   Refer to PostgreSQL configuration settings `listen` setting
+# @param pgsql_use_unix_socket
+#   Refer to PostgreSQL configuration settings `use_unix_socket` setting
+# @param pgsql_pgpass_path
+#   Refer to PostgreSQL configuration settings `pgpass_path` setting
+# @param pgsql_recovery_conf
+#   Refer to PostgreSQL configuration settings `recovery_conf` setting
+# @param pgsql_custom_conf
+#   Refer to PostgreSQL configuration settings `custom_conf` setting
+# @param pgsql_parameters
+#   Refer to PostgreSQL configuration settings `parameters` setting
+# @param pgsql_pg_hba
+#   Refer to PostgreSQL configuration settings `pg_hba` setting
+# @param pgsql_pg_ctl_timeout
+#   Refer to PostgreSQL configuration settings `pg_ctl_timeout` setting
+# @param pgsql_use_pg_rewind
+#   Refer to PostgreSQL configuration settings `use_pg_rewind` setting
+# @param pgsql_remove_data_directory_on_rewind_failure
+#   Refer to PostgreSQL configuration settings `remove_data_directory_on_rewind_failure` setting
+# @param pgsql_replica_method
+#   Refer to PostgreSQL configuration settings `replica_method` setting
+# @param use_consul
+#   Boolean to use Consul for configuration storage
+# @param consul_host
+#   Refer to Consul configuration `host` setting
+# @param consul_url
+#   Refer to Consul configuration `url` setting
+# @param consul_port
+#   Refer to Consul configuration `port` setting
+# @param consul_scheme
+#   Refer to Consul configuration `scheme` setting
+# @param consul_token
+#   Refer to Consul configuration `token` setting
+# @param consul_verify
+#   Refer to Consul configuration `verify` setting
+# @param consul_register_service
+#   Refer to Consul configuration `register_service` setting
+# @param consul_service_check_interval
+#   Refer to Consul configuration `service_check_interval` setting
+# @param consul_consistency
+#   Refer to Consul configuration `consistency` setting
+# @param consul_cacert
+#   Refer to Consul configuration `cacert` setting
+# @param consul_cert
+#   Refer to Consul configuration `cert` setting
+# @param consul_key
+#   Refer to Consul configuration `key` setting
+# @param consul_dc
+#   Refer to Consul configuration `dc` setting
+# @param consul_checks
+#   Refer to Consul configuration `checks` setting
+# @param use_etcd
+#   Boolean to use Etcd for configuration storage
+# @param etcd_host
+#   Refer to Etcd configuration `host` setting
+# @param etcd_hosts
+#   Refer to Etcd configuration `hosts` setting
+# @param etcd_url
+#   Refer to Etcd configuration `url` setting
+# @param etcd_proxyurl
+#   Refer to Etcd configuration `proxy` setting
+# @param etcd_srv
+#   Refer to Etcd configuration `srv` setting
+# @param etcd_protocol
+#   Refer to Etcd configuration `protocol` setting
+# @param etcd_username
+#   Refer to Etcd configuration `username` setting
+# @param etcd_password
+#   Refer to Etcd configuration `password` setting
+# @param etcd_cacert
+#   Refer to Etcd configuration `cacert` setting
+# @param etcd_cert
+#   Refer to Etcd configuration `cert` setting
+# @param etcd_key
+#   Refer to Etcd configuration `key` setting
+# @param use_exhibitor
+#   Boolean to use Exhibitor configuration storage
+# @param exhibitor_hosts
+#   Refer to Exhibitor configuration `hosts` setting
+# @param exhibitor_poll_interval
+#   Refer to Exhibitor configuration `poll_interval` setting
+# @param exhibitor_port
+#   Refer to Exhibitor configuration `port` setting
+# @param use_kubernetes
+#   Boolean to use Kubernetes configuration storage
+# @param kubernetes_namespace
+#   Refer to Kubernetes configuration `namespace` setting
+# @param kubernetes_labels
+#   Refer to Kubernetes configuration `labels` setting
+# @param kubernetes_scope_label
+#   Refer to Kubernetes configuration `scope_label` setting
+# @param kubernetes_role_label
+#   Refer to Kubernetes configuration `role_label` setting
+# @param kubernetes_use_endpoints
+#   Refer to Kubernetes configuration `use_endpoints` setting
+# @param kubernetes_pod_ip
+#   Refer to Kubernetes configuration `pod_ip` setting
+# @param kubernetes_ports
+#   Refer to Kubernetes configuration `ports` setting
+# @param restapi_connect_address
+#   Refer to REST API configuration `connect_address` setting
+# @param restapi_listen
+#   Refer to REST API configuration `listen` setting
+# @param restapi_username
+#   Refer to REST API configuration `username` setting
+# @param restapi_password
+#   Refer to REST API configuration `password` setting
+# @param restapi_certfile
+#   Refer to REST API configuration `certfile` setting
+# @param restapi_keyfile
+#   Refer to REST API configuration `keyfile` setting
+# @param restapi_cafile
+#   Refer to REST API configuration `cafile` setting
+# @param restapi_verify_client
+#   Refer to REST API configuration `verify_client` setting
+# @param use_zookeeper
+#   Boolean to enable Zookeeper configuration storage
+# @param zookeeper_hosts
+#   Refer to Zookeeper configuration `hosts` setting
+# @param watchdog_mode
+#   Refer to Watchdog configuration `mode` setting
+# @param watchdog_device
+#   Refer to Watchdog configuration `device` setting
+# @param watchdog_safety_margin
+#   Refer to Watchdog configuration `safety_margin` setting
+# @param package_name
+#   Patroni package name, only used when `install_method` is `package`
+# @param version
+#   Version of Patroni to install
+# @param install_dependencies
+#   Install dependencies, only used when `install_method` is `pip`
+# @param manage_python
+#   Manage Python class, only used when `install_method` is `pip`
+# @param install_method
+#   Install method
+# @param install_dir
+#   Install directory, only used when `install_method` is `pip`
+# @param python_class_version
+#   The  version of Python to pass to Python class
+#   Only used when `install_method` is `pip`
+# @param python_venv_version
+#   The  version of Python to pass to Python virtualenv defined type
+#   Only used when `install_method` is `pip`
+# @param config_path
+#   Path to Patroni configuration file
+# @param config_owner
+#   Patroni configuration file owner
+# @param config_group
+#   Patroni configuration file group
+# @param config_mode
+#   Patroni configuration file mode
+# @param service_name
+#   Name of Patroni service
+# @param service_ensure
+#   Patroni service ensure property
+# @param service_enable
+#   Patroni service enable property
+#
 class patroni (
 
   # Global Settings
