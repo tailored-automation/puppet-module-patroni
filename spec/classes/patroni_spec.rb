9b7f74120f5287195ac7584a77a64d149d0e5e1a
pmayekar <pmayekar@opentable.com>
2022-03-03 17:19:56 +0530

    adding standby cluster conig

--- spec/classes/patroni_spec.rb
+++ spec/classes/patroni_spec.rb
@@ -304,6 +304,23 @@ describe 'patroni' do
         )
       end
 
+      context 'is_standby => true' do
+        let(:params) { { 'scope' => 'testscope', 'is_standby' => true } }
+
+        it 'has valid config' do
+          content = catalogue.resource('file', 'patroni_config').send(:parameters)[:content]
+          config = YAML.safe_load(content)
+          expected = {
+            'standby_cluster' => {
+              'host' => '127.0.0.1',
+              'port' => '5432',
+              'primary_slot_name' => 'patroni',
+            },
+          }
+          expect(config).to include(expected)
+        end
+      end
+
       context 'use_etcd => true' do
         let(:params) { { 'scope' => 'testscope', 'use_etcd' => true } }
 
